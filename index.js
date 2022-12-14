cee9706ae1d7d3e08135e0c6675893d0c41956b6
Arockiajeyson <aarockiajeyson007@gmail.com>
2022-12-14 12:33:17 +0530

    backend-code

---
+++ index.js
@@ -0,0 +1,21 @@
+const express =require('express')
+
+const app =express()
+
+const dotenv =require('dotenv')
+
+const routes=require('./routes/routes')
+
+const DataBase=require('./dataBase/mongoDb')
+
+dotenv.config()
+
+DataBase()
+
+app.use(express.json({limit:"5mb"}))
+
+app.use('/',routes)
+
+app.listen(process.env.PORT,()=>{
+    console.log("port connected")
+})
---
+++ routes/routes.js
@@ -0,0 +1,44 @@
+const cloudinary = require('../cloudinary/cloudinary')
+
+const express = require('express')
+
+const route = express()
+
+const postSchema = require('../schema/schema')
+
+route.get('/getPost', async (req, res) => {
+    const data = await postSchema.find()
+    // console.log(data)
+    res.status(200).json(data)
+})
+
+route.post('/createPost', async (req, res) => {
+    try {
+
+        const {name,location,likes,description,PostImage,date}=req.body
+
+        const imgage = await cloudinary.uploader.upload(PostImage,{
+            folder:"img-folder"
+        })
+
+        const data =await postSchema.create({
+            name:name,
+            location:location,
+            likes:likes,
+            description:description,
+            PostImage:imgage.secure_url,
+            date:date
+        })
+        data.save()
+        res.status(200).json({
+            message:"Post Saved"
+        })
+
+    } catch (e) {
+        return res.json({
+            status: e.message
+        })
+    }
+})
+
+module.exports = route;
