5cb6f75e832cceaecfb3446f9630460142100780
Arockiajeyson <aarockiajeyson007@gmail.com>
2022-12-15 21:25:50 +0530

    likes updata

--- schema/schema.js
+++ schema/schema.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema
 const cloneModel = new Schema({
     name: { type: String },
     location: { type: String },
-    likes: { type: Number, default: 0 },
+    likes: { type: Number, default: Math.floor(Math.random()*100000) },
     description: { type: String },
     PostImage: { type: String },
     date: { type: String, default: new Date().toLocaleDateString() }
