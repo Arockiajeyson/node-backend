00c8414b756456afcccd888b0a0b9dc6c405b201
Arockiajeyson <aarockiajeyson007@gmail.com>
2022-12-15 11:12:39 +0530

    uploaded

--- cloudinary/cloudinary.js
+++ cloudinary/cloudinary.js
@@ -1,8 +1,9 @@
 var cloudinary = require('cloudinary').v2;
 
 cloudinary.config({ 
-    cloud_name: process.env.CLOUD_NAME, 
-    api_key: process.env.API_KEY, 
-    api_secret: process.env.API_SCRE
+    cloud_name: 'djx4fvpwq',//process.env.CLOUD_NAME, 
+    api_key: '644941413397474',//process.env.API_KEY, 
+    api_secret: 'DSsfrlUpLywll4GBdeoK590kWck'//process.env.API_SCRE
 });
+
 module.exports=cloudinary
--- routes/routes.js
+++ routes/routes.js
@@ -4,33 +4,50 @@ const express = require('express')
 
 const route = express()
 
+const fileupload = require('express-fileupload')
+
 const postSchema = require('../schema/schema')
 
+route.use(express.json({limit:"5mb"}))
+
+route.use(fileupload({ useTempFiles: true }));
+
 const body=require('body-parser')
+
+route.use(express.urlencoded())
+
+route.use(body.urlencoded({extended:true}))
+
 route.use(body.json())
+
+
 route.get('/getPost', async (req, res) => {
     const data = await postSchema.find()
-    // console.log(data)
+   
     res.status(200).json(data)
 })
 
 route.post('/createPost', async (req, res) => {
     try {
+        const img =req.files.PostImage.tempFilePath
+        
+        const {name,location,description}=req.body
 
-        const {name,location,likes,description,PostImage,date}=req.body
-
-        const imgage = await cloudinary.uploader.upload(PostImage,{
-            folder:"img-folder"
+        const imgage = await cloudinary.uploader.upload(img ,{
+            public_id: `${Date.now()}`,
+            resource_type: "auto",
+            folder: "images",
         })
+      
 
-        const datas =await postSchema.create({
+        const data = await postSchema.create({
             name:name,
             location:location,
             description:description,
             PostImage: imgage.secure_url,
         })
-        // console.log(datas)
-        datas.save()
+        
+        data.save()
         res.status(200).json({
             message:"Post Saved"
         })
